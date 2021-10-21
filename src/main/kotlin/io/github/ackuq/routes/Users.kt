7187989dddb3f300185e57697fd808c89019e332
Ackuq@ <axel@pettersson.cc>
2021-10-21 20:30:23 +0200

    Added tests, formatting and other fixes

--- src/main/kotlin/io/github/ackuq/routes/Users.kt
+++ src/main/kotlin/io/github/ackuq/routes/Users.kt
@@ -12,38 +12,52 @@ import io.ktor.http.*
 import io.ktor.routing.*
 
 
+fun Route.users() {
+    authenticate("admin") {
+        route("") {
+            get {
+                val users = UserController.getAllUsers()
+                handleApiSuccess(users, HttpStatusCode.OK, call)
+            }
+        }
+    }
+}
+
+fun Route.user() {
+    authenticate {
+        route("/{uuid}") {
+            get {
+                val user = call.principal<User>() ?: throw AuthorizationException("Invalid credentials")
+                val uuid = call.parameters["uuid"] ?: throw BadRequestException("No UUID specified")
+                if (user.uuid != uuid && user.role != Role.Admin) {
+                    throw AuthorizationException("Not authorized to see this page")
+                } else {
+                    val uuidUser = UserController.getUserByUUID(uuid)
+                    handleApiSuccess(uuidUser, HttpStatusCode.OK, call)
+                }
+            }
+        }
+    }
+}
+
+fun Route.me() {
+    authenticate {
+        route("/me") {
+            get {
+                val user = call.principal<User>() ?: throw AuthorizationException("Invalid credentials")
+                handleApiSuccess(user, HttpStatusCode.OK, call)
+            }
+        }
+    }
+}
+
+
 fun Application.usersRoutes() {
     routing {
-        route("/users") {
-            authenticate("admin") {
-                route("/") {
-                    get {
-                        val users = UserController.getAllUsers()
-                        handleApiSuccess(users, HttpStatusCode.OK, call)
-                    }
-                }
-            }
-
-            authenticate {
-                route("/me/") {
-                    get {
-                        val user = call.principal<User>() ?: throw AuthorizationException("Invalid credentials")
-                        handleApiSuccess(user, HttpStatusCode.OK, call)
-                    }
-                }
-                route("/{uuid}/") {
-                    get {
-                        val user = call.principal<User>() ?: throw AuthorizationException("Invalid credentials")
-                        val uuid = call.parameters["uuid"] ?: throw BadRequestException("No UUID specified")
-                        if (user.uuid != uuid && user.role != Role.Admin) {
-                            throw AuthorizationException("Not authorized to see this page")
-                        } else {
-                            val uuidUser = UserController.getUserByUUID(uuid)
-                            handleApiSuccess(uuidUser, HttpStatusCode.OK, call)
-                        }
-                    }
-                }
-            }
+        route("api/v1/users") {
+            users()
+            user()
+            me()
         }
     }
 }
