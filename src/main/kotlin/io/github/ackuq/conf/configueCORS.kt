e256da29ef5500a690e931e45000ad013cf1c543
Ackuq@ <axel@pettersson.cc>
2021-10-25 16:17:32 +0200

    Remove custom header

--- src/main/kotlin/io/github/ackuq/conf/configueCORS.kt
+++ src/main/kotlin/io/github/ackuq/conf/configueCORS.kt
@@ -11,7 +11,6 @@ fun Application.configureCORS() {
         method(HttpMethod.Delete)
         method(HttpMethod.Patch)
         header(HttpHeaders.Authorization)
-        header("MyCustomHeader")
         allowCredentials = true
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
