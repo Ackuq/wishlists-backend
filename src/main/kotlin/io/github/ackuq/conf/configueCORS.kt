c2802ec561c61e25d43a9dd8a025ff7449b7aa29
Ackuq@ <axel@pettersson.cc>
2021-10-25 16:20:27 +0200

    Add more CORS headers

--- src/main/kotlin/io/github/ackuq/conf/configueCORS.kt
+++ src/main/kotlin/io/github/ackuq/conf/configueCORS.kt
@@ -7,10 +7,15 @@ import io.ktor.http.*
 fun Application.configureCORS() {
     install(CORS) {
         method(HttpMethod.Options)
+        method(HttpMethod.Get)
+        method(HttpMethod.Post)
         method(HttpMethod.Put)
         method(HttpMethod.Delete)
         method(HttpMethod.Patch)
         header(HttpHeaders.Authorization)
+        header(HttpHeaders.AccessControlAllowHeaders)
+        header(HttpHeaders.ContentType)
+        header(HttpHeaders.AccessControlAllowOrigin)
         allowCredentials = true
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
